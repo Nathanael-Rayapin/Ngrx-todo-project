f080a9d7f9125a62bd3ef2a29c693eea49b959b7
Nathanael421 <nathanael.rayapin@gmail.com>
2022-06-08 14:46:51 +0200

    deleted files

--- src/app/todo/toto.model.ts
+++
@@ -1,7 +0,0 @@
-export class Todo {
-    constructor(public name: string, public id?: number) {
-        if (id === undefined) {
-            this.id = Math.floor(Date.now() * Math.random());
-        }
-    }
-}
